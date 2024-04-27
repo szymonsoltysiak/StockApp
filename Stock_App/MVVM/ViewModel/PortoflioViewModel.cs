cbfb3be4a17934760960918b93c65344974a6025
Soltyssss08 <263495@student.pwr.edu.pl>
2024-04-27 15:37:19 +0200

    Portfolio view

--- Stock_App/MVVM/ViewModel/PortoflioViewModel.cs
+++ Stock_App/MVVM/ViewModel/PortoflioViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Stock_App.MVVM.ViewModel
 {
+
     public class PortoflioViewModel : Core.ViewModel
     {
+        private readonly ObservableCollection<StockListItemViewModel> _stockListItemVM;
+        public IEnumerable<StockListItemViewModel> StockListItemVM => _stockListItemVM;
+
+        public PortoflioViewModel() 
+        {
+            _stockListItemVM = new ObservableCollection<StockListItemViewModel>();
+            _stockListItemVM.Add(new StockListItemViewModel("AAPL", 12.36));
+            _stockListItemVM.Add(new StockListItemViewModel("MSFT", 22.17));
+            _stockListItemVM.Add(new StockListItemViewModel("NVDA", 112.36));
+        }
     }
 }
---
+++ Stock_App/MVVM/ViewModel/StockListItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Stock_App.MVVM.ViewModel
+{
+    public class StockListItemViewModel : Core.ViewModel
+    {
+        public string Ticker { get; set; }
+        public double Price { get; set; }
+        public ICommand EditCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public StockListItemViewModel(string ticker, double price) 
+        {
+            Ticker = ticker;
+            Price = price;
+        }
+    }
+}
